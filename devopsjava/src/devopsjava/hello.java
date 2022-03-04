686efd87f8ab3d509b22a0c435f8e83b82a1bbb2
SumanthSrungavrapu <sumanthsrungavarapu3@gmail.com>
2022-03-04 17:03:19 +0530

    change by lacal1 branch

--- devopsjava/src/devopsjava/hello.java
+++ devopsjava/src/devopsjava/hello.java
@@ -5,6 +5,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Helloo world");
+		System.out.print("this is a change made in local1 branch");
+
 
 	}
 
