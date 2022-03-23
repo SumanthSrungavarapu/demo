18acd814cd7248c23da2244ec2fdb9f9a39b04cb
SumanthSrungavarapu <83802852+SumanthSrungavarapu@users.noreply.github.com>
2022-03-23 16:33:01 +0530

    Update hello.java

--- devopsjava/src/devopsjava/hello.java
+++ devopsjava/src/devopsjava/hello.java
@@ -1,10 +1,11 @@
 package devopsjava;
-
-public class hello {
+class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Helloo world");
+
+
 
 	}
 
