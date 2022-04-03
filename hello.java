48ea0c0d56a80d1cbb63a1adc828a6d51003674f
SumanthSrungavarapu <83802852+SumanthSrungavarapu@users.noreply.github.com>
2022-04-03 11:33:51 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello woorld");
+		System.out.print("Hello world");
 	}
 }
